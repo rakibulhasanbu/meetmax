71c44ea417b2952ecfccb177934d99f049f20233
rakibulhasanbu <rakibulhasanbu@gmail.com>
2024-08-20 20:13:32 +0600

    fix error

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,13 +17,14 @@ const provider = new GoogleAuthProvider();
 const auth = getAuth();
 
 export const authWithGoogle = async () => {
-    let user = null;
-    await signInWithPopup(auth, provider)
-        .then(result => {
-            user = result?.user
-        }).catch(err => {
-            console.log(err);
-        });
+  let user = null;
+  await signInWithPopup(auth, provider)
+    .then((result) => {
+      user = result?.user;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-    return user;
-}
+  return user;
+};
